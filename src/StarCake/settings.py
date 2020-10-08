36dc42f1200507a45eae468be34974e106038905
fahiz <fahizkppullara@gmail.com>
2020-10-08 11:09:10 +0530

    setup TinyMCE

--- src/StarCake/settings.py
+++ src/StarCake/settings.py
@@ -62,7 +62,7 @@ TEMPLATES = [
 # TinyMCE
 TINYMCE_DEFAULT_CONFIG = {
     'height': 360,
-    'width': 1120,
+    'width': 800,
     'cleanup_on_startup': True,
     'custom_undo_redo_levels': 20,
     'selector': 'textarea',
