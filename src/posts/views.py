0ea057199a76fb6272322abfb4e4736c7a5bdf5d
fahiz <fahizkppullara@gmail.com>
2020-10-08 07:33:51 +0530

    Navigator

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 from django.shortcuts import render
 from .models import Post
 from  marketing.models import Signup
@@ -19,7 +20,24 @@ def index(request):
 
 
 def blog(request):
-    return render(request,"blog.html",{})
+    most_recent = Post.objects.order_by('-timestamp')[:3]
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list,4)
+    #page request variable
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        paginated_queryset = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_queryset = paginator.page(1)
+    except EmptyPage:
+        paginated_queryset = paginator.page(paginator.num_pages)
+    context = {
+        'queryset' :paginated_queryset,
+        'most_recent': most_recent,
+        'page_request_var':page_request_var,
+    }
+    return render(request,"blog.html",context)
 
 def post(request):
     return render(request,"post.html",{})
